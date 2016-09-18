d45720665ed29b42b43a84c2255374844632ac6f
Antony Repin <lehcode@gmail.com>
2016-09-18 21:47:56 +0300

    modified:   website/core/DocsSidebar.js

--- website/core/DocsSidebar.js
+++ website/core/DocsSidebar.js
@@ -13,90 +13,102 @@ var React = require('React');
 var Metadata = require('Metadata');
 
 var DocsSidebar = React.createClass({
-  getCategories: function() {
-    var metadatas = Metadata.files.filter(function(metadata) {
-      return metadata.layout === 'docs' || metadata.layout === 'autodocs';
-    });
+    getCategories: function () {
+        var metadatas = Metadata.files.filter(function (metadata) {
+            return metadata.layout === 'docs' || metadata.layout === 'autodocs';
+        });
 
-    // Build a hashmap of article_id -> metadata
-    var articles = {};
-    for (var i = 0; i < metadatas.length; ++i) {
-      var metadata = metadatas[i];
-      articles[metadata.id] = metadata;
-    }
-
-    // Build a hashmap of article_id -> previous_id
-    var previous = {};
-    for (var i = 0; i < metadatas.length; ++i) {
-      var metadata = metadatas[i];
-      if (metadata.next) {
-        if (!articles[metadata.next]) {
-          throw '`next: ' + metadata.next + '` in ' + metadata.id + ' doesn\'t exist';
+        // Build a hashmap of article_id -> metadata
+        var articles = {};
+        for (var i = 0; i < metadatas.length; ++i) {
+            var metadata = metadatas[i];
+            articles[metadata.id] = metadata;
         }
-        previous[articles[metadata.next].id] = metadata.id;
-      }
+
+        // Build a hashmap of article_id -> previous_id
+        var previous = {};
+        for (var i = 0; i < metadatas.length; ++i) {
+            var metadata = metadatas[i];
+            if (metadata.next) {
+                if (!articles[metadata.next]) {
+                    throw '`next: ' + metadata.next + '` in ' + metadata.id + ' doesn\'t exist';
+                }
+                previous[articles[metadata.next].id] = metadata.id;
+            }
+        }
+
+        // Find the first element which doesn't have any previous
+        var first = null;
+        for (var i = 0; i < metadatas.length; ++i) {
+            var metadata = metadatas[i];
+            if (!previous[metadata.id]) {
+                first = metadata;
+                break;
+            }
+        }
+
+        var categories = [];
+        var currentCategory = null;
+
+        var metadata = first;
+        var i = 0;
+        while (metadata && i++ < 1000) {
+            if (!currentCategory || metadata.category !== currentCategory.name) {
+                currentCategory && categories.push(currentCategory);
+                currentCategory = {
+                    name: metadata.category,
+                    links: []
+                };
+            }
+            currentCategory.links.push(metadata);
+            metadata = articles[metadata.next];
+        }
+        categories.push(currentCategory);
+
+        return categories;
+    },
+
+    getLink: function (metadata) {
+        return metadata.permalink;
+    },
+
+    getDonationForm: function(categoryName){
+        if (categoryName === 'Основы'){
+            return <div className="nav-docs-section">
+                <h3>Поддержите переводчика</h3>
+                <div>
+                    <iframe src="https://funding.webmoney.ru/widgets/vertical/15512811-ff28-45b6-ae5d-85b0a7ea9638?hc=1&hs=1&sum=10" width="240" height="190" scrolling="no" style={{border:'none'}}></iframe>
+                </div>
+            </div>
+        }
+    },
+
+    render: function () {
+        return <div className="nav-docs">
+            <div className="nav-docs-viewport">
+                {this.getCategories().map((category) =>
+                    <div className="nav-docs-section" key={category.name}>
+                        <h3>{category.name}</h3>
+                        {console.log(category.name)}
+                        <ul>
+                            {category.links.map((metadata) =>
+                                <li key={metadata.id}>
+                                    <a
+                                        target={metadata.permalink.match(/^https?:/) && '_blank'}
+                                        style={{marginLeft: metadata.indent ? 20 : 0}}
+                                        className={metadata.id === this.props.metadata.id ? 'active' : ''}
+                                        href={this.getLink(metadata)}>
+                                        {metadata.title}
+                                    </a>
+                                </li>
+                            )}
+                        </ul>
+                        {this.getDonationForm(category.name)}
+                    </div>
+                )}
+            </div>
+        </div>;
     }
-
-    // Find the first element which doesn't have any previous
-    var first = null;
-    for (var i = 0; i < metadatas.length; ++i) {
-      var metadata = metadatas[i];
-      if (!previous[metadata.id]) {
-        first = metadata;
-        break;
-      }
-    }
-
-    var categories = [];
-    var currentCategory = null;
-
-    var metadata = first;
-    var i = 0;
-    while (metadata && i++ < 1000) {
-      if (!currentCategory || metadata.category !== currentCategory.name) {
-        currentCategory && categories.push(currentCategory);
-        currentCategory = {
-          name: metadata.category,
-          links: []
-        };
-      }
-      currentCategory.links.push(metadata);
-      metadata = articles[metadata.next];
-    }
-    categories.push(currentCategory);
-
-    return categories;
-  },
-
-  getLink: function(metadata) {
-    return metadata.permalink;
-  },
-
-  render: function() {
-    return <div className="nav-docs">
-      <div className="nav-docs-viewport">
-        {this.getCategories().map((category) =>
-          <div className="nav-docs-section" key={category.name}>
-            <h3>{category.name}</h3>
-            {console.log(category.name)}
-            <ul>
-              {category.links.map((metadata) =>
-                <li key={metadata.id}>
-                  <a
-                    target={metadata.permalink.match(/^https?:/) && '_blank'}
-                    style={{marginLeft: metadata.indent ? 20 : 0}}
-                    className={metadata.id === this.props.metadata.id ? 'active' : ''}
-                    href={this.getLink(metadata)}>
-                    {metadata.title}
-                  </a>
-                </li>
-              )}
-            </ul>
-          </div>
-        )}
-      </div>
-    </div>;
-  }
 });
 
 module.exports = DocsSidebar;
